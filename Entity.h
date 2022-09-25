83ef796faa80c1c0fa5855b3b36f5657041f15ea
CJReese01 <the_calvin@icloud.com>
2022-09-24 20:57:41 -0600

    modified:   Entity.h

--- Entity.h
+++ Entity.h
@@ -1,7 +1,15 @@
 class Entity{
     public:
-        int strength;
-        
+        short int get_stat(short int select);
+        void set_stat(short int select, short int stat);
+        void set_stats(short int stats[6]);
+        short int get_saving_throw(short int stat);
+        void set_saving_throw(short int stats[6]);
     private:
-
+        short int str;
+        short int dex;
+        short int con;
+        short int inte;
+        short int wis;
+        short int cha;
 };
